6b08ecb4f0f04aca0040735cae750333bc5458ea
asaad <abraham.saad@gmail.com>
2023-04-02 22:15:30 -0500

    first commit

---
+++ src/main/java/com/uninorte/distributed/programming/filemanagementservice/FileManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uninorte.distributed.programming.filemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementServiceApplication.class, args);
+	}
+
+}
